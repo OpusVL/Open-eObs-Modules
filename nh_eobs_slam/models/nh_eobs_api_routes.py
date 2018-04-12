f6baa206e7b439b8c276239866a652c2bb4688e8
James Curtis <james.curtis@opusvl.com>
2018-04-12 10:07:15 +0100

    [FIX][O1529] No escalation route for a zero score

--- nh_eobs_slam/models/nh_eobs_api_routes.py
+++ nh_eobs_slam/models/nh_eobs_api_routes.py
@@ -167,7 +167,9 @@ class NhEobsApiRoutes(orm.AbstractModel):
     def _get_case(self, obs):
         score = obs.score
         case = ""
-        if 0 < score <= 4:
+        if score == 0:
+            case = "case_0"
+        elif 0 < score <= 4:
             case = "case_1"
         elif 5 <= score <= 6:
             case = "case_3"
@@ -185,6 +187,15 @@ class NhEobsApiRoutes(orm.AbstractModel):
 
         task_tree = {
             "nurse": {
+                "case_0": [
+                    # {
+                    #     "model": "nh.clinical.notification.frequency",
+                    #     "fields": {
+                    #         "observation": "nh.clinical.patient.observation.ews",
+                    #         "frequency": False,
+                    #     },
+                    # },
+                ],
                 "case_1": [
                     {
                         "model": "nh.clinical.notification.shift_coordinator",
@@ -248,6 +259,9 @@ class NhEobsApiRoutes(orm.AbstractModel):
                 ]
             },
             "hca": {
+                "case_0": [
+                    "inform_nurse"
+                ],
                 "case_1": [
                     "inform_nurse"
                 ],
