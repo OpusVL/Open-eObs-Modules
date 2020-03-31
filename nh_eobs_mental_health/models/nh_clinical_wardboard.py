6acdcefb9137ff549a60029e966a8438d475003e
Peter Alabaster <peter.alabaster@opusvl.com>
2020-03-31 09:24:09 +0100

    [I84] Set bg next diff in new block. And rework from elif to if
    It doesn't make sense to handle refusing general obs and refusing
    bg obs as an if --> elif decision. If a patient is refusing
    both, then both need to be displayed. Thus changing the elif to if

--- nh_eobs_mental_health/models/nh_clinical_wardboard.py
+++ nh_eobs_mental_health/models/nh_clinical_wardboard.py
@@ -320,7 +320,8 @@ class NHClinicalWardboard(orm.Model):
                 elif spell.get('refusing_obs'):
                     rec['frequency'] = 'Refused - {0}'.format(rec['frequency'])
                     rec['next_diff'] = 'Refused - {0}'.format(rec['next_diff'])
-                elif spell.get('refusing_obs_blood_glucose'):
+                if spell.get('refusing_obs_blood_glucose'):
+                    rec['bg_frequency'] = 'Refused - {0}'.format(rec['bg_frequency'])
                     rec['next_blood_glucose_diff'] = 'Refused - {0}'.format(rec['next_blood_glucose_diff'])
         if was_single_record:
             return res[0]
